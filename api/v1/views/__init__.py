fa4ffed55e83cd0cc35c18f811037bb00a5f80e8
sachihiroo <saif3antar@gmail.com>
2024-04-21 05:09:11 +0100

    script

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,5 @@ API Blueprint .
 entry point for the Flask application.
 """
 from flask import Blueprint
-from api.v1.views.index import *
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+from api.v1.views.index import *
