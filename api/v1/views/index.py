765e4ba7365c26ba10d994b0c8e5addff31cbead
sachihiroo <saif3antar@gmail.com>
2024-04-21 04:57:12 +0100

    Some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,13 @@ It contains routes for retrieving status information and other functionalities.
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", strict_slashes=False)
@@ -17,3 +24,21 @@ def status():
     """
     json_data = {"status": "OK"}
     return jsonify(json_data)
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """
+    Get statistics about the API.
+
+    - endpoint that retrieves the number of each objects by type
+    """
+    objs = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+    return jsonify(objs)
